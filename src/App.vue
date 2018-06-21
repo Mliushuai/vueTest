763bad2de30aa4c0530bf24fbef53df3ed3726bd
liushuai <shuai.liu@aseit.cn>
2018-06-21 11:45:55 +0800

    修改布局 刚开始  ....

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,45 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+   <header>
+     这是脑袋
+   </header>
+<div class="menu-con">
+111
+</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App'
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  html,body{height:100%;margin:0px;}
+  #app {
+    position: relative;
+    overflow: hidden;
+    height:100%;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+header{
+  float: right;
+  width: 80%;
+  height: 60px;
+  background-color: blue;
 }
+  .menu-con{
+    float: left;
+    width: 20%;
+    height: 100%;
+    overflow: hidden;
+    background-color: aquamarine;
+  }
+
+
 </style>
