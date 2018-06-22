f9feb907f7897736b227dc787e143aedad6b5af0
liushuai <shuai.liu@aseit.cn>
2018-06-22 17:35:53 +0800

    完成接口测试

--- src/BaseApi/index.js
+++ src/BaseApi/index.js
@@ -1,7 +1,8 @@
 /**
- *
+ *心茹  从这改你自己用的接口加好了
  */
-const ajaxBaseUrl = 'http://192.168.2.9:11110/api/industry/'
+const ajaxBaseUrl = 'http://192.168.2.9:11110/api/industry/';
+// const ajaxBaseUrl ='http://60.191.78.148:12000'
 
 export default
 {
